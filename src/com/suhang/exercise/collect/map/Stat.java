46487a22d2020b7936dc571df87a9409496a2b2a
hang.su <tomhon@qq.com>
2017-03-31 18:16:18 +0800

    add stat

---
+++ src/com/suhang/exercise/collect/map/Stat.java
@@ -0,0 +1,12 @@
+package com.suhang.exercise.collect.map;
+
+/**
+ * @author hang.su
+ * @since 2017-03-31 18:09
+ */
+public class Stat {
+
+    public static void main(String[] args) {
+
+    }
+}
